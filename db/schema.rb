1baf8639e65715ac8042e1b786572d874f24157a
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-03-12 15:58:15 -0500

    working more on roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312193354) do
+ActiveRecord::Schema.define(version: 20160312205152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,10 @@ ActiveRecord::Schema.define(version: 20160312193354) do
     t.integer  "location_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
+  add_index "rooms", ["category_id"], name: "index_rooms_on_category_id", using: :btree
   add_index "rooms", ["location_id"], name: "index_rooms_on_location_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -100,5 +102,6 @@ ActiveRecord::Schema.define(version: 20160312193354) do
   add_foreign_key "companies", "users"
   add_foreign_key "locations", "companies"
   add_foreign_key "photos", "rooms"
+  add_foreign_key "rooms", "categories"
   add_foreign_key "rooms", "locations"
 end
