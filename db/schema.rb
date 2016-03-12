6f345bb46013057c5391097c192f77add543f630
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-03-12 11:25:09 -0500

    added categories resource
    categories have a name and description

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312153255) do
+ActiveRecord::Schema.define(version: 20160312162427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
