058726e48a5b4a414b2f7637fbfb652c902a6383
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-03-12 21:23:49 -0500

    got more seed data up
    also put some conditionals in the views to not show empty fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Need to seed some Locations as well as Rooms
 
 User.delete_all
+Company.delete_all
+
+# Users
 User.create(
   email: 'admin@escapeworld.us',
   password: 'password',
@@ -18,3 +15,77 @@ User.create(
   password: 'password',
   role: 'owner'
 )
+
+# Companies
+
+Company.create(
+  name: 'Escape the Room Atlanta',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'Global Escape Room',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'Locked In',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'Mission Escape',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'ParanoiaQuest',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'Room Escape Atlanta',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'Ultimate Escape Game',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'Urban Escape Game',
+  city: 'Atlanta',
+  state: 'Georgia',
+)
+
+Company.create(
+  name: 'Amazing Escape Room',
+  street: '5825 Glenridge Dr Building 2, Suite 217',
+  city: 'Atlanta',
+  state: 'Georgia',
+  description: 'Experience this new and thrilling game with your friends',
+  website: 'http://amazingescaperoom.com',
+  email: 'support@amazingescaperoom.com',
+  phone: '404-480-3852'
+)
+
+Company.create(
+  name: 'Breakout Atlanta Escape Games',
+  city: 'Atlanta',
+  state: 'Georgia',
+  description: 'Hands on Escape experience this likes of which you have never seen',
+  website: 'http://breakoutatlanta.com',
+  phone: '404-620-2477'
+)
+
+# Locations @TODO
+
+# Rooms @TODO
