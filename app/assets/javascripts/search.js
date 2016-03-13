782c69e7793ad34a105ee7e8cc42d8692f614062
Nick Touchette <touchette@gmail.com>
2016-03-13 04:12:07 -0400

    updated search with api to location API

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,44 +1,47 @@
 $(document).ready(function(){
-  var substringMatcher = function(strs) {
-    return function findMatches(q, cb) {
-      var matches, substringRegex;
-
-      // an array that will be populated with substring matches
-      matches = [];
-
-      // regex used to determine if a string contains the substring `q`
-      substrRegex = new RegExp(q, 'i');
-
-      // iterate through the pool of strings and for any string that
-      // contains the substring `q`, add it to the `matches` array
-      $.each(strs, function(i, str) {
-        if (substrRegex.test(str)) {
-          matches.push(str);
-        }
-      });
-
-      cb(matches);
-    };
-  };
-
-  var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-  ];
-
+  var dataset;
   $('.typeahead').typeahead({
     hint: true,
     highlight: true,
-    minLength: 1
+    minLength: 3
   },
   {
-    name: 'states',
-    source: substringMatcher(states)
+    name: 'cities',
+    source: function(query, syncResults, asyncResults) {
+
+      if (query.length >= 3 && query.length <= 5) {
+        $.ajax({
+          url: 'http://gd.geobytes.com/AutoCompleteCity?callback=?&q=' + query,
+          dataType: "jsonp",
+        })
+        .done(function(data) {
+          dataset = data;
+          asyncResults(data);
+        });
+      } else {
+        syncResults(dataset);
+      }
+    }
   });
+  $('.typeahead').on('blur', function () {
+    alert("test");
+    ev = $.Event("keydown");
+    ev.keyCode = ev.which = 40;
+    $('.typeahead').trigger(ev);
+    return true;
+  });
+
+  // function getcitydetails(fqcn) {
+  //   if (typeof fqcn == "undefined") fqcn = jQuery("#f_elem_city").val();
+  //   cityfqcn = fqcn;
+  //   if (cityfqcn) {
+  //       jQuery.getJSON(
+  //                   "http://gd.geobytes.com/GetCityDetails?callback=?&fqcn="+cityfqcn,
+  //                      function (data) {
+  //               jQuery("#geobyteslatitude").val(data.geobyteslatitude);
+  //               jQuery("#geobyteslongitude").val(data.geobyteslongitude);
+  //               }
+  //       );
+  //   }
+  // }
 });
