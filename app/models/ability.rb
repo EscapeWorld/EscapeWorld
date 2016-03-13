4ce864f939f8a3db876f3756262382c5280dc40d
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-03-12 19:07:41 -0500

    i think i got roles working

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,16 @@ class Ability
 
   def initialize(user)
     user ||= User.new
+
+    # Permissions for our users role
     if user.has_role? :admin
       can :access, :rails_admin
       can :dashboard
-    else
-      cannot :access, :rails_admin
+      can :manage, :all
+    elsif user.has_role? :owner
+      can :access, :rails_admin
+      can :dashboard
     end
+
   end
 end
