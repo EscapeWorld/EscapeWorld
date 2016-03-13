5ec9d72647245de990891b0c8e0202eeb17b82be
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-03-12 21:04:59 -0500

    got geolocation home to work
    hacked on the bourbon / bitters javascript to get html5 geolocation data through dynamically

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,54 +1,91 @@
-window.onload = function() {
-  var bittersMap = (function () {
-    var myLatlng = new google.maps.LatLng(59.342457, 18.057340),
-        mapCenter = new google.maps.LatLng(59.340458, 18.057340),
-        mapCanvas = document.getElementById('map_canvas'),
-        mapOptions = {
-          center: mapCenter,
-          zoom: 13,
-          scrollwheel: false,
-          draggable: true,
-          disableDefaultUI: true,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        },
-        map = new google.maps.Map(mapCanvas, mapOptions),
-        contentString =
-          '<div id="content">'+
-          '<div id="siteNotice">'+
-          '</div>'+
-          '<h1 id="firstHeading" class="firstHeading">thoughtbot</h1>'+
-          '<div id="bodyContent"'+
-          '<p>Sveavägen 98</p>'+
-          '</div>'+
-          '</div>',
-        infowindow = new google.maps.InfoWindow({
-          content: contentString,
-          maxWidth: 300
-        }),
-        marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: 'thoughtbot (Sweden)'
-        });
+var bittersMap = function (geo) {
+  var myLatlng = new google.maps.LatLng(geo.lat, geo.lon),
+      mapCenter = new google.maps.LatLng(geo.lat, geo.lon),
+      mapCanvas = document.getElementById('map_canvas'),
+      mapOptions = {
+        center: mapCenter,
+        zoom: 13,
+        scrollwheel: false,
+        draggable: true,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      },
+      map = new google.maps.Map(mapCanvas, mapOptions),
+      contentString =
+        '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">home</h1>'+ // this needs to be dynamic
+        '<div id="bodyContent"'+
+        '<p>Atlanta, Ga</p>'+ // This is going to need to be dynamic somehow
+        '</div>'+
+        '</div>',
+      infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 300
+      }),
+      marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'home'
+      });
 
-    return {
-      init: function () {
-        map.set('styles', [{
-          featureType: 'landscape',
-          elementType: 'geometry',
-          stylers: [
-            { hue: '#ffff00' },
-            { saturation: 30 },
-            { lightness: 10}
-          ]}
-        ]);
+  return {
+    init: function () {
+      map.set('styles', [{
+        featureType: 'landscape',
+        elementType: 'geometry',
+        stylers: [
+          { hue: '#ffff00' },
+          { saturation: 30 },
+          { lightness: 10}
+        ]}
+      ]);
 
-        google.maps.event.addListener(marker, 'click', function () {
-          infowindow.open(map,marker);
-        });
-      }
-    };
-  }());
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map,marker);
+      });
+    },
 
-  bittersMap.init();
+    mark: function (geo) {
+      // good
+      var myLatlng = new google.maps.LatLng(geo.lat, geo.lon),
+      // good
+      home = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'home'
+      });
+
+      google.maps.event.addListener(home, 'click', function () {
+        console.log('map is ok?');
+        infowindow.open(map,home);
+      });
+
+      console.log(geo);
+    }
+  };
 };
+
+function success (position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+
+  // build our own object
+  var geoData = {
+    lat: lat,
+    lon: lon
+  }
+
+  // build bitters map
+  bittersMap(geoData).init();
+}
+
+function error () {
+  console.log('didnt get data')
+}
+
+// put the whole thing in motion!
+window.onload = function () {
+  navigator.geolocation.getCurrentPosition(success, error)
+}
